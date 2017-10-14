a5bde4c5b88c22eda7bc59749b619965b688ec02
lgoldsten <goldstenliam@gmail.com>
2017-10-14 01:11:50 -0400

    added RouteForm

--- dev/js/RouteForm.jsx
+++ dev/js/RouteForm.jsx
@@ -1,22 +1,38 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
 
 import { Header } from 'semantic-ui-react';
 
-export default class AllenhubPage extends React.Component {
+export default class RouteForm extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
 
         };
-
-        this.bindThisToFunctionsPassedAsParameters();
-    }
-
-    bindThisToFunctionsPassedAsParameters() {
-
     }
 
     render() {
-        return <Header>RouteForm</Header>
+        return (
+            <Form>
+                <Form.Field>
+                    <label> Begin your trip at: </label>
+                    <Input 
+                        placeholder = 'Where you are' 
+                        onChange = {this.props.onStartLocationChange}
+                        content = {this.props.startLocation}/>
+                </Form.Field>
+                <Form.Field>
+                    <label> End your trip at: </label>
+                    <Input 
+                        placeholder = 'Where you want to go' 
+                        onChange = {this.props.onEndLocationChange}
+                        content = {this.props.endLocation}/>
+                </Form.Field>
+                <Button
+                    type = 'submit' 
+                    onClick = {this.props.onSubmit}> 
+                    Let's Go! </Button>
+            </Form>
+        )
     }
 }
