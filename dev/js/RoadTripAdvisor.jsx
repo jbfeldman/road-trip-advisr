5f6a17f0ba9bc08efafcf8b96ca12b9de12f6395
Nathan Allen <nathan@allenhub.com>
2017-10-14 14:02:29 -0400

    got it working somehow

--- dev/js/RoadTripAdvisor.jsx
+++ dev/js/RoadTripAdvisor.jsx
@@ -23,27 +23,37 @@ export default class RoadTripAdvisor extends React.Component {
     }
 
     render() {
-        // console.log(this.state);
-        // if (this.state.readyForPlanner && this.state.end && this.state.start) {
+        if (this.state.readyForPlanner && this.state.end && this.state.start) {
             return (
-                <Planner
-                    endLocation={{lat: 42.3601, lng: -71.0589}}
-                    startLocation={{lat: 42.1015, lng: -72.5898}}
-                    // endLocation={this.state.end}
-                    // startLocation={this.state.start}
-                />
+                <div>
+                    <RouteForm
+                        endLocation={this.state.end}
+                        onEndLocationSet={this.setEndLocation}
+                        onStartLocationSet={this.setStartLocation}
+                        onSubmit={this.setReadyForPlanner}
+                        startLocation={this.state.start}
+                    />
+                    <Planner
+                        // endLocation={{lat: 42.3601, lng: -71.0589}}
+                        // startLocation={{lat: 42.1015, lng: -72.5898}}
+                        endLocation={this.state.end}
+                        startLocation={this.state.start}
+                    />
+                </div>
             );
-        // } else {
-        //     return (
-        //         <RouteForm
-        //             endLocation={this.state.end}
-        //             onEndLocationSet={this.setEndLocation}
-        //             onStartLocationSet={this.setStartLocation}
-        //             onSubmit={this.setReadyForPlanner}
-        //             startLocation={this.state.start}
-        //         />
-        //     );
-        // }
+        } else {
+            return (
+                <div>
+                    <RouteForm
+                        endLocation={this.state.end}
+                        onEndLocationSet={this.setEndLocation}
+                        onStartLocationSet={this.setStartLocation}
+                        onSubmit={this.setReadyForPlanner}
+                        startLocation={this.state.start}
+                    />
+                </div>
+            );
+        }
     }
 
     setReadyForPlanner() {
